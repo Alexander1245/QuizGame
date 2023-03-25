ab062e6c4563d75a6969014e6e14d3b35281a76d
Alexander Myasnikov <al.next235@gmail.com>
2023-03-25 16:06:11 +0300

    Add navigate up button to the toolbar

--- app/src/main/java/com/dart69/quizgame/main/presentation/MainActivity.kt
+++ app/src/main/java/com/dart69/quizgame/main/presentation/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.dart69.quizgame.main.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.dart69.quizgame.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,5 +13,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
